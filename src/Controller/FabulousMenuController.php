41e6caee6bcfe6c2f4f030166c748032500a941a
Robin Caudy <robin.caudy@gmail.com>
2021-02-17 11:59:31 +0100

    First wiew created and partially implemented. Next step is to display the content of the database.

--- src/Controller/FabulousMenuController.php
+++ src/Controller/FabulousMenuController.php
@@ -9,20 +9,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class FabulousMenuController extends AbstractController
 {
     /**
-     * @Route("/fabulous/menu", name="fabulous_menu")
+     * @Route("/", name="menu")
      */
-    public function index(): Response
+    public function menu()
     {
-        return $this->render('fabulous_menu/index.html.twig', [
+        return $this->render('fabulous_menu/menu.html.twig');
+    }
+    
+    /**
+     * @Route("/menu/edit", name="menu_edit")
+     */
+    public function menu_edit(): Response
+    {
+        return $this->render('fabulous_menu/menu_edit.html.twig', [
             'controller_name' => 'FabulousMenuController',
         ]);
-    }
-
-    /**
-     * @Route("/", name="home")
-     */
-    public function home()
-    {
-        return $this->render('fabulous_menu/home.html.twig');
     }
 }
